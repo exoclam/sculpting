eda0ae68e08fed537ea3a1d42c8ff603b5980699
Chris <chris@Chriss-MacBook-Air.local>
2021-06-09 14:36:52 -0400

    added hipergator code directory and simulate script

---
+++ hipergator/simulate.py
@@ -0,0 +1,369 @@
+import json
+import sys
+import numpy as np
+from numpy import log, exp, pi
+import pandas as pd
+import scipy
+import scipy.stats as stats
+import random
+from scipy.stats import gaussian_kde, loguniform
+from math import lgamma
+from glob import glob
+import os
+import seaborn as sns
+import matplotlib as mpl
+#import matplotlib.pyplot as plt
+#%matplotlib inline
+
+path = '/blue/sarahballard/c.lam/sculpting/'
+berger_kepler = pd.read_csv(path+'berger_kepler_stellar17.csv') # crossmatched with Gaia via Bedell
+pnum = pd.read_csv(path+'pnum_plus_cands.csv')
+pnum = pnum.drop_duplicates(['kepid'])
+k = pnum.koi_count.value_counts() 
+k = pd.Series([len(berger_kepler)-np.sum(k), 244, 51, 12, 8, 1]) 
+
+def sim_transits_new(r_star, m_star, num_planets, mu, sigma, r_planet, age_star, planets_per_case2,
+    planets_a_case2, inclinations, inclinations_degrees, impact_parameters, transit_statuses,
+    transit_status1, transit_status2, transit_multiplicities, tdurs, cdpp, sns, prob_detections):
+    """
+    In a loop through Kepler data, keep info on stellar radius, age, and CDPP. 
+    But we sample number of planets and their periods, draw midplane for the system, 
+    draw inclination from the midplane for each planet, convert period to a, calculate impact parameter b, 
+    calculate transit duration, calculate normalized transit duration xi (aka mutual inclination proxy),
+    classify each planet as transiting or not
+    
+    Parameters
+    ----------
+    r_star: radius of the star in solar radii
+    m_star: mass of the star in solar masses
+    num_planets: number of planets sampled based on stellar age check
+    mu: planetary disk midplane in radians
+    sigma: sigma used in drawing planet inclinations from around the midplane; 2 degrees
+    r_planet: radius of the planet (not a variable for now; 2 R_Earth)
+    age_star: age of the star
+    cdpp: 6-hour CDPP of planet
+
+    possible **kwargs are empty arrays:
+    planets_per_case2
+    inclinations
+    inclinations_degrees
+    planets_a_case2
+    impact_parameters
+    transit_status
+    transit_multiplicities
+    tdurs
+    sns (signal noise ratios)
+    
+    Returns
+    -------
+    None: populates a bunch of different lists set up before the loop in which function is called,
+    including those for inclination, period, semi-major axis, impact parameter, transit duration,
+    transit status, transit multiplicity, S/N, and Fressin detection probabilities
+    
+    """
+
+    # draw planet periods from loguniform distribution according to num_planets
+    planet_per_case2 = list(loguniform.rvs(2, 300, size=num_planets)) 
+    planets_per_case2.append(planet_per_case2) 
+
+    # inclinations for impact parameter formula
+    inclination = list(np.random.normal(mu, sigma, num_planets))
+    #print("mu and sigma: ", mu, sigma)
+    inclination = [np.pi/2 if inc_elt > np.pi/2 else inc_elt for inc_elt in inclination] # artificially impose bounds post-facto
+    inclination = [-np.pi/2 if inc_elt < -np.pi/2 else inc_elt for inc_elt in inclination] # lower bound
+    #print("i: ", inclination)
+    inclinations.append(inclination) 
+    inclinations_degrees.append([inc*180/np.pi for inc in inclination]) 
+
+    # semi-major axes based on Kepler's Third Law
+    #planet_a_case2 = [day_to_year(ppc2)**(2./3) for ppc2 in planet_per_case2]
+    #planet_a_case2 = [au_to_solar_radius(day_to_year(ppc2)**(2./3)) for ppc2 in planet_per_case2]
+    planet_a_case2 = [p_to_a(ppc2, m_star) for ppc2 in planet_per_case2] # in AU
+    planet_a_case2_solar_radius = [au_to_solar_radius(p_to_a(ppc2, m_star)) for ppc2 in planet_per_case2] # in solar radii
+    planets_a_case2.append(planet_a_case2) 
+
+    # calculate impact parameters; distance units in solar radii
+    b = (planet_a_case2_solar_radius * np.sin(inclination))/r_star
+    impact_parameters.append(list(b))
+    
+    # note individual planets' *geometric* transit status; ignores S/N
+    ts1 = [1 if np.abs(param) <= 1. else 0 for param in b]
+    transit_status1.append(ts1)
+    
+    # calculate transit durations using Winn 2011 formula; same units as period
+    #print("period: ", planet_per_case2)
+    #print("b: ", b)
+    #print("semi-major axis: ", planet_a_case2)
+    #print("i: ", inclination)
+    tdur = transit_duration(planet_per_case2, solar_radius_to_au(r_star), 
+                            earth_radius_to_au(r_planet), b, planet_a_case2, inclination) 
+    #print("TDUR: ", tdur)
+    tdurs.append(tdur) # days
+    
+    # draw jmags based on age-jmag relation, given age
+    #jmag = build_and_sample_from_2dpdf(berger_kepler.iso_age, berger_kepler.jmag, age_star)
+    #jmags.append(jmag)
+    
+    # calculate S/N using Christiansen et al 2012 Eqn 4; input
+    sn = calculate_sn(planet_per_case2, r_planet, r_star, cdpp, tdur) # time units in days
+    sns.append(sn)
+    
+    # calculate Fressin detection probability based on S/N
+    #ts2 = [1 if sn_elt >= 7.1 else 0 for sn_elt in sn] # S/N threshold before Fressin et al 2013
+    prob_detection = np.array([0.1*(sn_elt-6) for sn_elt in sn]) # S/N threshold using Fressin et al 2013
+    prob_detection[np.isnan(prob_detection)] = 0 # replace NaNs with zeros
+    prob_detection = np.where(prob_detection < 0., 0., prob_detection) # replace negative probs with zeros
+    # actually, replace all probabilities under 5% with 5% to avoid over-penalizing models which terminate at 0% too early
+    prob_detection = np.where(prob_detection > 1, 1, prob_detection) # replace probs > 1 with just 1
+    prob_detections.append(prob_detection)
+    
+    # sample transit status based on Fressin detection probability
+    #transit_status = [ts1_elt * ts2_elt for ts1_elt, ts2_elt in zip(ts1, ts2)]
+    transit_status = [np.random.choice([1, 0], p=[pd, 1-pd]) for pd in prob_detection]
+    transit_statuses.append(transit_status)
+    
+    # transit check based on solely on impact parameters, ie. solely on geometry, not noise limit
+    transit_multiplicities.append(len([ts for ts in transit_status if ts == 1]))
+    #transit_multiplicities.append(len([param for param in b if np.abs(param) <= 1.]))
+
+def model_direct_draw(cube):
+    """
+    Take prior and draw systems out of Kepler data 
+    Params: n-dim hypercube prior
+    Returns: lambdas and simulated transit DataFrame
+    """
+    # retrieve prior
+    #print(cube)
+    m, b = cube[0], cube[1]
+    
+    kepids = []
+    midplanes = []
+    midplanes_degrees = []
+    inclinations = []
+    inclinations_degrees = []
+    planets_per_case2 = [] # maybe better to recreate it here b/c I can then generalize it for Case 2?
+    planets_a_case2 = []
+    star_radius = []
+    star_mass = []
+    impact_parameters = []
+    transit_status1 = []
+    transit_status2 = []
+    transit_statuses = []
+    transit_multiplicities = []
+    tdurs = []
+    jmags = []
+    sns = []
+    prob_detections = []
+    xi = []
+    xi_old = []
+    xi_young = []
+    intacts = 0
+    
+    # draw ~20000 systems
+    num_samples = 10
+    for i in range(len(berger_kepler[0:num_samples])):
+    #for i in range(10):
+        # star
+        kepids.append(berger_kepler.kepid[i])
+        r_star = berger_kepler.iso_rad[i] # in solar radii
+        m_star = berger_kepler.iso_mass[i] # in solar masses
+        age_star = berger_kepler.iso_age[i]
+        mu = list(np.random.uniform(-np.pi/2,np.pi/2,1)) # create midplane for star 
+        midplanes.append(mu)
+        midplanes_degrees.append([mu_elt*180/np.pi for mu_elt in mu])
+        cdpp = berger_kepler.rrmscdpp06p0[i] # don't convert from ppm to parts per unit
+
+        # planet
+        r_planet = 2. # use two Earth radii; will make negligible difference
+        prob = m * age_star + b # calculate probability given age from sampled distribution
+        if prob < 0.: # don't allow negative probabilities
+            prob = 0.
+        intact_flag = np.random.choice(['intact', 'disrupted'], p=[prob, 1-prob])
+        if intact_flag == 'intact':
+            intacts += 1
+            # young system has 5 or 6 planets
+            num_planets = random.choice([5, 6]) 
+            sigma = np.pi/90 # 2 degrees, per Fig 6 in Fabrycky 2012
+            
+            # simulate transit-related characteristics for 5 or 6 planets
+            sim_transits_new(r_star, m_star, num_planets, mu, sigma, r_planet, age_star, 
+                             planets_per_case2 = planets_per_case2, planets_a_case2 = planets_a_case2, 
+                             inclinations = inclinations, inclinations_degrees = inclinations_degrees,
+                             impact_parameters = impact_parameters, transit_statuses = transit_statuses, 
+                             transit_status1 = transit_status1, transit_status2 = transit_status2, 
+                             transit_multiplicities = transit_multiplicities, tdurs = tdurs,
+                             cdpp = cdpp, sns = sns, prob_detections = prob_detections)
+
+        elif intact_flag == 'disrupted':
+            # old system has 1 or 2 planets
+            num_planets = random.choice([1, 2]) 
+            sigma = np.pi/22.5 # 8 degree disk plane inclination scatter
+            
+            # simulate transit-related characteristics for 1 or 2 planets
+            sim_transits_new(r_star, m_star, num_planets, mu, sigma, r_planet, age_star, 
+                             planets_per_case2 = planets_per_case2, planets_a_case2 = planets_a_case2, 
+                             inclinations = inclinations, inclinations_degrees = inclinations_degrees, 
+                             impact_parameters = impact_parameters, transit_statuses = transit_statuses, 
+                             transit_status1 = transit_status1, transit_status2 = transit_status2,
+                             transit_multiplicities = transit_multiplicities, tdurs = tdurs,
+                             cdpp = cdpp, sns = sns, prob_detections = prob_detections)
+
+    midplanes = np.concatenate(midplanes, axis=0) # turn list of lists of one into regular list
+    intact_fractions = intacts/num_samples
+    #print(intacts, num_samples, intact_fractions)
+    
+    """
+    transits_dict = {'star_ages': berger_kepler.iso_age, 'planet_periods': planets_per_case2, 
+    'semi_major_axes': planets_a_case2, 'midplane': midplanes, 'midplane_degrees': midplanes_degrees,
+                     'planet_inclinations': inclinations, 'planet_inclinations_degrees': inclinations_degrees,
+                     'impact_parameters': impact_parameters, 'transit_status': transit_statuses, 
+                     'transit_multiplicity': transit_multiplicities, 'kepid': kepids,
+                     'y_intercept': b, 'slope': m, 'transit_duration': tdurs, 
+                     '6hr_cdpp': berger_kepler.rrmscdpp06p0, 'signal_noise': sns,
+                     'prob_detections': prob_detections}
+    """
+    transits_dict = {'star_ages': berger_kepler.iso_age[0:num_samples], 'planet_periods': planets_per_case2[0:num_samples], 
+    'semi_major_axes': planets_a_case2[0:num_samples], 'midplane': midplanes[0:num_samples], 'midplane_degrees': midplanes_degrees[0:num_samples],
+                     'planet_inclinations': inclinations[0:num_samples], 'planet_inclinations_degrees': inclinations_degrees[0:num_samples],
+                     'impact_parameters': impact_parameters[0:num_samples], 'transit_status': transit_statuses[0:num_samples], 
+                     'transit_multiplicity': transit_multiplicities[0:num_samples], 'kepid': kepids[0:num_samples],
+                     'y_intercept': b, 'slope': m, 'transit_duration': tdurs[0:num_samples], 
+                     '6hr_cdpp': berger_kepler.rrmscdpp06p0[0:num_samples], 'signal_noise': sns[0:num_samples],
+                     'prob_detections': prob_detections[0:num_samples]}
+
+    transits = pd.DataFrame(transits_dict)    
+    transits.to_csv('sims/transits_direct_draw.csv')
+    
+    # formerly normalized by total of k, but now I care about zero-counts, so no need to normalize anymore
+    #lam = transits.transit_multiplicity.value_counts()
+    #lam = transits.loc[transits.transit_multiplicity > 0].transit_multiplicity.value_counts() * (np.sum(k_old)/len(transits.loc[transits.transit_multiplicity > 0]))
+    lam = transits.loc[transits.transit_multiplicity > 0].transit_multiplicity.value_counts() * (len(berger_kepler)/num_samples) # scale up to full counts of k
+    #print(lam)
+    #print(transits.transit_multiplicity.value_counts())
+    
+    return lam, transits, intact_fractions
+
+def loglike_direct_draw(cube, ndim, nparams):
+    """
+    Poisson log likelihood from synthetic-posteriors.ipynb
+
+    Returns: Poisson log-likelihood
+    """
+
+    # retrieve prior cube and feed prior-normalized hypercube into model to generate transit multiplicities
+    lam, _, intact_fractions = model_direct_draw(cube)
+    #print("MODEL: ", lam)
+    #print("DATA: ", k) # from Berger et al 2020 crossmatched with Gaia via Bedell's kepler-gaia.fun
+    
+    term1 = np.sum(k) * np.log(lam)
+    term2 = -np.sum(lam)
+    term3 = -np.sum([lgamma(kth + 1) for kth in k])
+    poisson_loglikelihood = term1 + term2 + term3
+    #print("POISSON: ", poisson_loglikelihood)
+    return poisson_loglikelihood, lam, intact_fractions
+
+def transit_duration(P, r_star, r_planet, b, a, inc): # Winn 2011 Eqn 14
+    #print(P, r_star, r_planet, b, a, inc)
+    arg1 = np.sqrt((1+(r_planet/r_star))**2 - b**2)
+    #print("arg1: ", arg1)
+    arg2 = (r_star / np.array(a)) * (arg1 / np.sin(np.pi/2 - np.array(inc))) # was sin(i) but need to adjust for different index
+    #arg2 = (r_star / np.array(a)) * arg1
+    #print("arg2: ", arg2)
+    
+    return (np.array(P) / np.pi) * np.arcsin(arg2)
+
+def p_to_a(p, M):
+    # Newton's full version of Kepler's Third Law, assuming planet mass m --> 0
+    p = p*86400 # days to seconds
+    M = M*1.989e30 # solar mass to kg
+    a_in_meters = (((p**2) * 6.67e-11 * M)/(4*np.pi**2))**(1./3) # SI units in the house
+    a = a_in_meters/(1.496e11) # meters to AU
+    #print(p/86400, M/1.989e30, a)
+    return a # in AU
+
+def calculate_xi(tdur1, tdur2, P1, P2): # Fabrycky 2012 Eqn 1
+    arg1 = tdur1/(P1**(1./3))
+    arg2 = tdur2/(P2**(1./3))
+    return arg1 / arg2
+
+def day_to_year(per):
+    return per/365.24 
+
+def solar_radius_to_au(radius):
+    return 0.00465047*radius
+
+def au_to_solar_radius(au):
+    return 215.032*au
+
+def earth_radius_to_au(radius):
+    return 4.26352e-5*radius
+
+def calculate_sn(P, rp, rs, cdpp, tdur): 
+    """
+    Calculate S/N per planet using Eqn 4 in Christiansen et al 2012: https://arxiv.org/pdf/1208.0595.pdf
+    
+    Params: P (days); rp (Earth radii); rs (Solar radii); cdpp (ppm); tdur (days)
+    
+    Returns: S/N
+    """
+    tobs = 365*3.5 # days; time spanned observing the target; set to 3.5 years, or the length of Kepler mission
+    f0 = 0.92 # fraction of time spent actually observing and not doing spacecraft things
+    tcdpp = 0.25 # days; using CDPP for 6 hour transit durations; could change to be more like Earth transiting Sun?
+    rp = earth_radius_to_au(rp)
+    rs = solar_radius_to_au(rs)
+    #print(P, rp, rs, cdpp, tdur)
+    
+    factor1 = np.sqrt(tobs*f0/np.array(P)) # this is the number of transits
+    delta = 1e6*(rp/rs)**2 # convert from parts per unit to ppm
+    cdpp_eff = cdpp * np.sqrt(tcdpp/tdur)
+    factor2 = delta/cdpp_eff
+    sn = factor1 * factor2
+    #print(factor1, delta, cdpp_eff, sn)
+
+    return sn
+
+def prior_grid(cube, ndim, nparams, gi_m, gi_b):
+    """
+    Each model run will use an evenly spaced (m,b) tuple on a discrete 11x11 grid
+    Does that ruin the point of multinest? Is it valid without random sampling? 
+    Maybe so, but pymultinest keeps getting stuck, so I need to get a better sense of the logL surface.
+    gi_m: grid index on m axis
+    gi_b: grid index on b axis
+    """
+    cube[0] = np.linspace(-1,0,11)[gi_m]
+    cube[1] = np.linspace(0,1,11)[gi_b]
+    return cube
+
+# set up "hypercube" prior and iterate through it while calling model and likelihood functions
+ndim = 2
+nparams = 2
+lams = []
+logLs = []
+intact_fracs = []
+ms = []
+bs = []
+for gi_m in range(11):
+    for gi_b in range(11):
+        temp_logLs = []
+        temp_lams = []
+        temp_intact_fracs = []
+        cube = [random.uniform(0,1), random.uniform(0,1)] # instantiate cube
+        cube = prior_grid(cube, ndim, nparams, gi_m, gi_b) # move to new position on cube
+        for i in range(10): # ideally should be more
+            # calculate logL by comparing model(cube) and k
+            logL, lam, intact_fractions = loglike_direct_draw(cube, ndim, nparams) 
+            lam = lam.to_list()
+            temp_lams.append(lam)
+            temp_logLs.append(logL)
+            temp_intact_fracs.append(intact_fractions)
+            
+        ms.append(round(cube[0],1))
+        bs.append(round(cube[1],1))
+        lams.append(temp_lams)
+        logLs.append(temp_logLs)
+        intact_fracs.append(temp_intact_fracs)
+        
+df = pd.DataFrame({'ms': ms, 'bs': bs, 'intact_fracs': intact_fracs, 'logLs': logLs, 'lams': lams})
+print(df)
+#lams.to_csv('lams_cands.csv', index=False)
+df.to_csv('/blue/sarahballard/c.lam/sculpting/simulations.csv', index=False, sep='\t')
